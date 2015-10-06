925c5f46362f023e32778e56f32ae75198f8003a
Eli Zucker <github@redytedy.33mail.com>
2015-10-06 11:35:43 -0700

    Improved the GameProcessor

--- src/GameProcessor.java
+++ src/GameProcessor.java
@@ -4,19 +4,18 @@ public class GameProcessor {
 	private Unit[][] map = new Unit[12][12];
 	private char[][] moveList = new char[12][12];
 	Gameboard board;
-	
+
 	public GameProcessor(Gameboard board) {
 		this.board=board;
-		
 		generateMap();
 	}
-	
+
 	public void generateMap() {
-		
+
 		boolean[][] spaceUsed = new boolean[12][12];
 		int fencesGenerated = 0;
 		int mhosGenerated = 0;
-		
+
 		//Fill the board with spaces and spikes on the edge
 		for (int i = 0; i < 12; i++) {
 			for (int j = 0; j < 12; j++) {
@@ -27,36 +26,36 @@ public class GameProcessor {
 				}
 			}
 		}
-		
+
 		//Generate 20 spikes in unique locations
 		while (fencesGenerated < 20) {
 			int x = 1+(int)(Math.random()*10);
 			int y = 1+(int)(Math.random()*10);
-			
+
 			if(spaceUsed[x][y]==false) {
 				map[x][y] = new Spike(x, y, board);
 				spaceUsed[x][y] = true;
 				fencesGenerated++;
 			}
 		}
-		
+
 		//Generate 12 mhos in unique locations
 		while (mhosGenerated < 12) {
 			int x = 1+(int)(Math.random()*10);
 			int y = 1+(int)(Math.random()*10);
-			
+
 			if(spaceUsed[x][y]==false) {
 				map[x][y] = new Mho(x, y, board);
 				spaceUsed[x][y] = true;
 				mhosGenerated++;
 			}
 		}
-		
+
 		//Generate a player in a unique location
 		while (true) {
 			int x = 1+(int)(Math.random()*10);
 			int y = 1+(int)(Math.random()*10);
-			
+
 			if(spaceUsed[x][y]==false) {
 				map[x][y] = new Player(x, y, board);
 				spaceUsed[x][y] = true;
@@ -64,28 +63,98 @@ public class GameProcessor {
 			}
 		}
 	}
-	
+
 	public Unit[][] getMap() {
 		return map;
 	}
-	
+
 	public char[][] getMoveList() {
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < 12; j++) {
-				moveList[i][j] = Legend.NO_MOVEMENT;
-			}
-		}
-		moveList[2][2] = Legend.DOWN_RIGHT;
+
 		return moveList;
 	}
-	
-	
+
+
 	//called by gameboard when animating is done
 	public void animatingDone() {
-		map[2][2] = new BlankSpace(2, 2, board);
-		map[3][3] = new Mho(3, 3, board);
+
+	}
+
+	private int[] getPlayerLocation() {
+		for (int i = 0; i < 12; i++)
+			for (int j = 0; j < 12; j++) 
+				if (map[i][j] instanceof Player)
+					return new int[]{i,j};
+		return null;
 	}
 	
+	private void gameOver() {
+		//still needs to be implemeneted
+	}
+
+	private boolean validPlayerMove(char moveType) {
+		int xOffset = 0;
+		int yOffset = 0;
+
+		switch(moveType) {
+		case Legend.UP: 
+			yOffset = -1;
+			break;
+		case Legend.UP_LEFT:
+			yOffset = -1;
+			xOffset = -1;
+			break;
+		case Legend.UP_RIGHT:
+			yOffset = -1;
+			xOffset = 1;
+			break;
+		case Legend.DOWN:
+			yOffset = 1;
+			break;
+		case Legend.DOWN_RIGHT:
+			yOffset = 1;
+			xOffset = 1;
+			break;
+		case Legend.DOWN_LEFT:
+			yOffset = 1;
+			xOffset = -1;
+			break;
+		case Legend.LEFT:
+			xOffset = -1;
+			break;
+		case Legend.RIGHT:
+			xOffset = 1;
+			break;
+		}
+
+		int i = getPlayerLocation()[0];
+		int j = getPlayerLocation()[1];
+
+		if (map[i][j] instanceof Player)
+			if (map[i+xOffset][j+yOffset] instanceof Spike || map[i+xOffset][j+yOffset] instanceof Mho) 
+				return false;
+
+		return true;
+
+	}
+
+	public void playerMove(char move) {
+		if(validPlayerMove(move)) {
+			moveList[getPlayerLocation()[0]][getPlayerLocation()[1]] = move;
+			for (int i = 0; i < 12; i++)
+				for (int j = 0; j < 12; j++)
+					if (map[i][j] instanceof Mho)
+						moveMho(i, j);
+		}
+		else {
+			gameOver();
+		}
+	}
 	
+	public void jump() {
+
+	}
 	
+	public void moveMho(int x, int y) {
+		//still to be implemented
+	}
 }
--- src/Gameboard.java
+++ src/Gameboard.java
@@ -9,22 +9,22 @@ import javax.swing.JPanel;
 
 
 public class Gameboard extends JPanel {
-	
+
 	//The number of milliseconds per change in frame - lower means faster
 	public final int ANIMATION_SPEED = 100;
-	
+
 	//Number of frames in each animation - higher means a smoother animation, but will take longer
 	public final int ANIMATION_INTERVALS = 10;
-	
+
 	//The current frame that the animation is on
 	private int animationFrame = -1;
-	
+
 	//Whether or not the board is currently in an animation phase
 	private boolean animating = false;
-	
+
 	//The GameProcessor object. This object will do all of the move processing behind the scenes
 	GameProcessor gameProcessor;
-	
+
 	public Gameboard() {
 		//Set the default size
 		setPreferredSize(new Dimension(700,700));
@@ -39,16 +39,16 @@ public class Gameboard extends JPanel {
 	 * Called whenever the state of animating has changed
 	 */
 	public void toggleAnimating() {
-		
+
 		//Signify to the GameProcessor that animating has finished
 		if(animating)
 			gameProcessor.animatingDone();
-		
+
 		animating = !animating;
-		
+
 		//Set the animation frame back to -1
 		animationFrame = -1;
-		
+
 		return;
 
 	}
@@ -60,7 +60,7 @@ public class Gameboard extends JPanel {
 	public int getAnimationFrame() {
 		return animationFrame;
 	}
-	
+
 	/**
 	 * 
 	 * @return true if currently animating, false if otherwise
@@ -68,7 +68,7 @@ public class Gameboard extends JPanel {
 	public boolean isAnimating() {
 		return animating;
 	}
-	
+
 	/**
 	 * 
 	 * @return the list of moves (animations) from the gameProcessor
@@ -76,43 +76,43 @@ public class Gameboard extends JPanel {
 	public char[][] getMoveList() {
 		return gameProcessor.getMoveList();
 	}
-	
+
 	/**
 	 * Override the paintComponent method in order to draw each object in the window
 	 */
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		//Checks if animation is finished, then calls toggleAnimating() if true
 		if(animationFrame == ANIMATION_INTERVALS) 
 			toggleAnimating();
-		
+
 		//Paint the background and game elements
 		paintBackground(g);
 		paintGameElements(g);
 
 
 	}
-	
+
 	/**
 	 * paints the grid in the background
 	 * @param g the Graphics object to paint with
 	 */
 	public void paintBackground(Graphics g) {
-		
+
 		g.setColor(Color.BLACK);
-		
+
 		//Loop in order to draw each line
 		for (int i = 1; i<12; i++) {
 			g.drawLine((int)(i*(getWidth()/12.0)), 0, (int)(i*(getWidth()/12.0)), getHeight());
 			g.drawLine(0, (int)(i*(getHeight()/12.0)), getWidth(), (int)(i*(getHeight()/12.0)));
 		}
-		
+
 	}
 
 	public void paintGameElements(Graphics g) {
-		
+
 		//Adds one more frame to animationFrame if animating
 		if(isAnimating()) {
 			animationFrame+=1;
@@ -122,9 +122,7 @@ public class Gameboard extends JPanel {
 		for (int i = 0; i < 12; i++)
 			for (int j = 0; j < 12; j++) 
 				gameProcessor.getMap()[i][j].paint(g);
-
 	}
-
 }
 
 
